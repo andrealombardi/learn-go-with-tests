0249f8cb4cdb252be27d2e19dab0e7ef79be7bc8
andrealombardi <andrea.lombardi@gmail.com>
2021-10-12 17:46:58 +0100

    Reflection, first part

---
+++ reflection/walk_test.go
@@ -0,0 +1,55 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{"Struct with two string field",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{"Struct with a string and an int",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 44},
+			[]string{"Chris"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string){
+				got = append(got, input)
+			})
+
+			if len(got) != len(test.ExpectedCalls) {
+				t.Errorf("wrong number of function calls, got %d want %d", len(got), len(test.ExpectedCalls))
+			}
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+
+		})
+	}
+
+
+}
