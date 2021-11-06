91af943a9932df00b32f4b0cd99cff5b9d486fc8
andrealombardi <andrea.lombardi@gmail.com>
2021-11-06 20:10:46 +0000

    Pass context as first argument

--- thecontext/server.go
+++ thecontext/server.go
@@ -1,17 +1,23 @@
 package thecontext
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, store.Fetch())
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return // todo: log error however you like
+		}
+
+		fmt.Fprint(w, data)
 	}
 }
 
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(ctx context.Context) (string, error)
 }
